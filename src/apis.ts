e3db1c16472f550583944fc696755c48a59450e1
chango <kimtothechang@gmail.com>
2022-12-24 21:49:17 +0900

    fix: rewrites로 인한 base_url 제거

--- src/apis.ts
+++ src/apis.ts
@@ -1,13 +1,11 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const BASE_URL = "https://www.year-end-party.site";
+// export const BASE_URL = "https://www.year-end-party.site";
 
 export const Api = {
   user: {
-    signup: (kakao_id: number, name: string) =>
-      axios.post(BASE_URL + "/api/user/signup", { kakao_id, name }),
-    login: (kakao_id: number, name: string) =>
-      axios.post(BASE_URL + "/api/user/login", { kakao_id, name }),
-    me: axios.get(BASE_URL + "/"),
+    signup: (kakao_id: number, name: string) => axios.post('/api/user/signup', { kakao_id, name }),
+    login: (kakao_id: number, name: string) => axios.post('/api/user/login', { kakao_id, name }),
+    // me: axios.get('/'),
   },
 };
