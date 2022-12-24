64888cf9f8c96bdcab9cf6297ef63b4d71def50a
chango <kimtothechang@gmail.com>
2022-12-24 20:38:46 +0900

    fix: url 충돌 제거

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    BASE_URL: process.env.BASE_URL,
-  },
+
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
+        source: '/:path*',
         destination: `https://www.year-end-party.site/:path*`,
       },
     ];
