d07f31841c04f94fdbe5b502e4790c15257c36da
chango <kimtothechang@gmail.com>
2022-12-24 20:44:22 +0900

    back: before cors setting

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: `https://www.year-end-party.site/:path*`,
-      },
-    ];
+  env: {
+    BASE_URL: process.env.BASE_URL,
   },
 };
 
