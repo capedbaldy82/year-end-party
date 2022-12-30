4b8d507bf98af1bd7ddb73371417f40dd5a007cb
chango <kimtothechang@gmail.com>
2022-12-30 14:31:01 +0900

    feat: table 페이지 SSR 적용

--- src/pages/[userId]/table.tsx
+++ src/pages/[userId]/table.tsx
@@ -1,18 +1,19 @@
-import styled from "@emotion/styled";
-import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
-import { useRecoilValue } from "recoil";
-import { userInfoState } from "../../atoms";
-import Button from "../../components/Button";
-import CheersTable from "../../components/CheersTable";
-import Description from "../../components/Description";
-import Layout from "../../components/Layout";
-import Spacer from "../../components/Spacer";
-import Title from "../../components/Title";
+import styled from '@emotion/styled';
+import { NextPageContext } from 'next';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import { userInfoState } from '../../atoms';
+import Button from '../../components/Button';
+import CheersTable from '../../components/CheersTable';
+import Description from '../../components/Description';
+import Layout from '../../components/Layout';
+import Spacer from '../../components/Spacer';
+import Title from '../../components/Title';
 
 const Container = styled.div``;
 
-function TablePage() {
+const TablePage = ({ username }: { username: string }) => {
   const router = useRouter();
   const userId = router.query.userId as string;
   const userInfo = useRecoilValue(userInfoState);
@@ -20,20 +21,22 @@ function TablePage() {
 
   const copyURL = () => {
     let currentUrl = window.document.location.href;
-    let t = document.createElement("textarea");
+    let t = document.createElement('textarea');
     document.body.appendChild(t);
-    t.value = currentUrl.replace("table", "excheers");
+    t.value = currentUrl.replace('table', 'excheers');
     t.select();
-    document.execCommand("copy");
+    document.execCommand('copy');
     document.body.removeChild(t);
 
-    alert("링크가 복사되었습니다. 친구에게 공유해보세요!");
+    alert('링크가 복사되었습니다. 친구에게 공유해보세요!');
   };
 
   const getPosts = async () => {
+    if (!userId) return;
     try {
       const response = await fetch(`/api/post/${userId}`);
       const res = await response.json();
+
       if (res.ok) {
         setPosts(res.posts);
       } else {
@@ -46,7 +49,7 @@ function TablePage() {
 
   useEffect(() => {
     getPosts();
-  }, []);
+  }, [userId]);
 
   const onClickExCheers = () => {
     router.push(`/${userId}/excheers`);
@@ -55,7 +58,7 @@ function TablePage() {
   return (
     <Layout>
       <Container>
-        <Title text="닉네임님의 건배사 테이블" />
+        <Title text={username !== 'error' ? `${username}님의 건배사 테이블` : '건배사 테이블'} />
         <Description text="친구들의 맞건배사 모음 공간입니다. :)" />
         <Spacer my={40} />
         <CheersTable posts={posts} />
@@ -68,6 +71,28 @@ function TablePage() {
       </Container>
     </Layout>
   );
-}
+};
 
 export default TablePage;
+
+export const getServerSideProps = async ({ query }: NextPageContext) => {
+  const dev = process.env.NODE_ENV !== 'production';
+  const url = dev ? 'http://localhost:3000' : 'https://www.year-end-party.site';
+
+  try {
+    const response = await fetch(`${url}/api/rollingpaper/${query.userId}`);
+    const res = await response.json();
+
+    return {
+      props: {
+        username: res.name,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        username: 'error',
+      },
+    };
+  }
+};
